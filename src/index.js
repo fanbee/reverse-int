7473f0b20106a1a7071c12fada04c853deff3d6d
Dzmitry Anishchuk <fantomwork@gmail.com>
2020-10-14 15:05:18 +0300

    feat: add all additional functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
-}
+    
+    const str = "" + Math.abs(n);      // convert absolute value to string
+    const reversed = str.split("")     // get array of digit characters
+                     .reverse()        // reverse it
+                     .join("");        // join into string again;
+    const num = +reversed;             // convert integer
+    return num;     
+    }
+
